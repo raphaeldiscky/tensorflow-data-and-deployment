af3154a1c3b8c98d70cd6813e7f36f90e83b44c3
Raphael Discky <zundria.putra@gmail.com>
2022-04-22 19:57:12 +0700

    Finish course 1

--- Course 1 - Browser-based Models with TensorFlow.js/Week 4/Graded/webcam.js
+++ Course 1 - Browser-based Models with TensorFlow.js/Week 4/Graded/webcam.js
@@ -17,14 +17,13 @@
  * A class that wraps webcam video elements to capture Tensor4Ds.
  */
 
-
 class Webcam {
   /**
    * @param {HTMLVideoElement} webcamElement A HTMLVideoElement representing the
    *     webcam feed.
    */
   constructor(webcamElement) {
-    this.webcamElement = webcamElement;
+    this.webcamElement = webcamElement
   }
 
   /**
@@ -32,25 +31,24 @@ class Webcam {
    * Returns a batched image (1-element batch) of shape [1, w, h, c].
    */
 
-
   capture() {
     return tf.tidy(() => {
       // Reads the image as a Tensor from the webcam <video> element.
-      const webcamImage = tf.browser.fromPixels(this.webcamElement);
+      const webcamImage = tf.browser.fromPixels(this.webcamElement)
 
-      const reversedImage = webcamImage.reverse(1);
+      const reversedImage = webcamImage.reverse(1)
 
       // Crop the image so we're using the center square of the rectangular
       // webcam.
-      const croppedImage = this.cropImage(reversedImage);
+      const croppedImage = this.cropImage(reversedImage)
 
       // Expand the outer most dimension so we have a batch size of 1.
-      const batchedImage = croppedImage.expandDims(0);
+      const batchedImage = croppedImage.expandDims(0)
 
       // Normalize the image between -1 and 1. The image comes in between 0-255,
       // so we divide by 127 and subtract 1.
-      return batchedImage.toFloat().div(tf.scalar(127)).sub(tf.scalar(1));
-    });
+      return batchedImage.toFloat().div(tf.scalar(127)).sub(tf.scalar(1))
+    })
   }
 
   /**
@@ -58,14 +56,13 @@ class Webcam {
    * @param {Tensor4D} img An input image Tensor to crop.
    */
 
-
   cropImage(img) {
-    const size = Math.min(img.shape[0], img.shape[1]);
-    const centerHeight = img.shape[0] / 2;
-    const beginHeight = centerHeight - (size / 2);
-    const centerWidth = img.shape[1] / 2;
-    const beginWidth = centerWidth - (size / 2);
-    return img.slice([beginHeight, beginWidth, 0], [size, size, 3]);
+    const size = Math.min(img.shape[0], img.shape[1])
+    const centerHeight = img.shape[0] / 2
+    const beginHeight = centerHeight - size / 2
+    const centerWidth = img.shape[1] / 2
+    const beginWidth = centerWidth - size / 2
+    return img.slice([beginHeight, beginWidth, 0], [size, size, 3])
   }
 
   /**
@@ -75,39 +72,46 @@ class Webcam {
    * @param {number} height The real height of the video element.
    */
 
-
   adjustVideoSize(width, height) {
-    const aspectRatio = width / height;
+    const aspectRatio = width / height
     if (width >= height) {
-      this.webcamElement.width = aspectRatio * this.webcamElement.height;
+      this.webcamElement.width = aspectRatio * this.webcamElement.height
     } else if (width < height) {
-      this.webcamElement.height = this.webcamElement.width / aspectRatio;
+      this.webcamElement.height = this.webcamElement.width / aspectRatio
     }
   }
 
   async setup() {
     return new Promise((resolve, reject) => {
-      navigator.getUserMedia = navigator.getUserMedia ||
-          navigator.webkitGetUserMedia || navigator.mozGetUserMedia ||
-          navigator.msGetUserMedia;
+      navigator.getUserMedia =
+        navigator.getUserMedia ||
+        navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia ||
+        navigator.msGetUserMedia
       if (navigator.getUserMedia) {
         navigator.getUserMedia(
-            {video: {width: 224, height: 224}},
-            stream => {
-              this.webcamElement.srcObject = stream;
-              this.webcamElement.addEventListener('loadeddata', async () => {
+          { video: { width: 224, height: 224 } },
+          (stream) => {
+            this.webcamElement.srcObject = stream
+            this.webcamElement.addEventListener(
+              'loadeddata',
+              async () => {
                 this.adjustVideoSize(
-                    this.webcamElement.videoWidth,
-                    this.webcamElement.videoHeight);
-                resolve();
-              }, false);
-            },
-            error => {
-              reject(error);
-            });
+                  this.webcamElement.videoWidth,
+                  this.webcamElement.videoHeight
+                )
+                resolve()
+              },
+              false
+            )
+          },
+          (error) => {
+            reject(error)
+          }
+        )
       } else {
-        reject();
+        reject()
       }
-    });
+    })
   }
 }
